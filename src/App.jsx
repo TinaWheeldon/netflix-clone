4950081627b96f42e5395b127b1a9ae34e24c774
TinaWheeldon <tina.wheeldon@gmail.com>
2025-10-27 15:57:03 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 
 const App = () => {
   return (
     <div>
-      
+      <Home/>
     </div>
   )
 }
