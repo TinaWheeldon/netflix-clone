c57bace240fafbf4fc4bd6c384d829750b7563d0
TinaWheeldon <tina.wheeldon@gmail.com>
2025-11-05 09:34:33 -0600

    Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './Footer.css'
 import youtube_icon from '../../assets/youtube_icon.png'
 import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assetsinstagram_icon.png'
-import facebook_icon from '../../assetsfacebook_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className="footer-icons">
-        <img src={youtube_icon.png} alt="" />
-        <img src={twitter_icon.png} alt="" />
-        <img src={instagram_icon.png} alt="" />
-        <img src={facebook_icon.png} alt="" />
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
       </div>
       <ul>
         <li>Audio Description</li>
