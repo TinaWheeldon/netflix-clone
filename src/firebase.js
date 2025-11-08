6a021a08c1a6a01f7ff8ef54480864ce49f91efd
TinaWheeldon <tina.wheeldon@gmail.com>
2025-11-08 13:20:57 -0600

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCEy6YUeAvjOmRARvTRdfGeL8RJDzs8U7A",
@@ -14,3 +14,36 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+    try {
+       const res = await createUserWithEmailAndPassword(auth, email, password);
+       const user = res.user;
+       await addDoc(collection(db, "user"), {
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+       });
+    }
+    catch (error){
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async () => {
+    try{
+        signInWithEmailAndPassword(auth, email, password);
+    }
+    catch(error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = () => {
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
